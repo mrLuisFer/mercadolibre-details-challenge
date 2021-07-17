1b9b7659c2e613b049148e288b6df5904a95dac5
mrLuisFer <mrluisfeer@gmail.com>
2021-07-16 20:34:12 -0500

    fix: sorting line cause is a warn with eslint

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { render } from 'react-dom'
-import './index.css'
+
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+import './index.css'
 
 render(
   <StrictMode>
@@ -11,7 +13,4 @@ render(
   document.getElementById('root')
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()

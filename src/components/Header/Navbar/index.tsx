39dbb11c06688eb559ad65441b8e985b9ce4ce76
mrLuisFer <mrluisfeer@gmail.com>
2021-07-17 23:14:38 -0500

    feat: adding some styles in navbar

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -1,7 +1,14 @@
 export default function Navbar() {
   return (
-    <div className="navbar" role="navigation">
-      <p>Navbar</p>
-    </div>
+    <nav className="navbar" role="navigation">
+      <div className="navbar-sendto">
+        <i className="fas fa-map-marker-alt navbar-sendto-icon" />
+        <p className="navbar-sendto-text">
+          <span>Enviar a</span> Córdoba 5822
+        </p>
+      </div>
+      <div>div 2</div>
+      <div>div 3</div>
+    </nav>
   )
 }

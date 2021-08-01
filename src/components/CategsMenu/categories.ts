0748ffa02b8157e3cdfcefc9f1a1849d9e16c2f6
mrLuisFer <mrluisfeer@gmail.com>
2021-08-01 18:53:59 -0500

    feat: adding styles and details in categsMenu

---
+++ src/components/CategsMenu/categories.ts
@@ -0,0 +1,90 @@
+export type TCategs = {
+  name: string
+  url: string
+  id?: number | string
+}
+
+export const categs: TCategs[] = [
+  {
+    url: 'https://www.mercadolibre.com.ar/c/hogar-muebles-y-jardin#menu=categories',
+    name: 'Vehiculos'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/hogar-muebles-y-jardin#menu=categories',
+    name: 'Inmuebles'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/hogar-muebles-y-jardin#menu=categories',
+    name: 'Tecnologia'
+  },
+
+  {
+    url: 'https://www.mercadolibre.com.ar/c/hogar-muebles-y-jardin#menu=categories',
+    name: 'Hogar y Muebles'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/electrodomesticos-y-aires-ac#menu=categories',
+    name: 'Electrodomésticos'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/herramientas#menu=categories',
+    name: 'Herramientas'
+  },
+
+  {
+    url: 'https://www.mercadolibre.com.ar/c/construccion#menu=categories',
+    name: 'Construcción'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/deportes-y-fitness#menu=categories',
+    name: 'Deportes y Fitness'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/accesorios-para-vehiculos#menu=categories',
+    name: 'Accesorios para Vehículos'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/ropa-y-accesorios#menu=categories',
+    name: 'Moda'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/juegos-y-juguetes#menu=categories',
+    name: 'Juegos y Juguetes'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/bebes#menu=categories',
+    name: 'Bebés'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/belleza-y-cuidado-personal#menu=categories',
+    name: 'Belleza y Cuidado Personal'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/salud-y-equipamiento-medico#menu=categories',
+    name: 'Salud y Equipamiento Médico'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/industrias-y-oficinas#menu=categories',
+    name: 'Industrias y Oficinas'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/agro#menu=categories',
+    name: 'Agro'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/productos-sustentables#menu=categories',
+    name: 'Productos Sustentables'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/servicios#menu=categories',
+    name: 'Servicios'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/tiendas-oficiales#menu=categories',
+    name: 'Tiendas oficiales'
+  },
+  {
+    url: 'https://www.mercadolibre.com.ar/c/hogar-muebles-y-jardin#menu=categories',
+    name: 'Ver mas categorias'
+  }
+]
--- src/components/CategsMenu/index.tsx
+++ src/components/CategsMenu/index.tsx
@@ -1,20 +1,47 @@
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
+
+import { categs, TCategs } from './categories'
 
 type Props = {
   setIsRenderMenu: Dispatch<SetStateAction<Boolean>>
 }
 
 export default function CategsMenu({ setIsRenderMenu }: Props) {
+  const [showTechMenu, setShowTechMenu] = useState<Boolean>(false)
+  const techCateg = 'tecnologia'
+
+  const renderTechMenu = (category: TCategs) => {
+    if (category.name.toLowerCase() === techCateg) {
+      setShowTechMenu(true)
+    }
+  }
+
   return (
     <div className="categsMenu-container">
       <div className="categsMenu" onMouseLeave={() => setIsRenderMenu(false)}>
-        <p>Some</p>
-        <p>Some</p>
-        <p>Some</p>
-        <p>Some</p>
-        <p>Some</p>
-        <p>Some</p>
+        <ul className="categsMenu-categories">
+          {categs?.map((category) => (
+            <li
+              key={category.name}
+              className="categsMenu-categories-item"
+              onMouseEnter={() => renderTechMenu(category)}
+            >
+              <a
+                className="categsMenu-categories-item-link"
+                href={category.url}
+                rel="noreferrer"
+                target="_blank"
+              >
+                {category.name}
+                {category.name.toLowerCase() === techCateg && (
+                  <i className="fas fa-chevron-right arrow" />
+                )}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
+      {showTechMenu && <div>Menu</div>}
     </div>
   )
 }

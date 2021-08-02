9ffdd756d21d3c7e76be8ab53d949e4c2bd2d828
mrLuisFer <mrluisfeer@gmail.com>
2021-08-01 19:00:58 -0500

    fix: theme in manifest
    fix: move Routes to src/ dir

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
 import Header from '../components/Header'
-
-import Routes from './routes'
+import Routes from '../routes'
 
 const App = () => {
   return (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route, Redirect } from 'react-router-dom'
 
-import DetailsScreen from '../../product/screens/Details'
-import HomeScreen from '../screens/Home'
+import DetailsScreen from '../product/screens/Details'
+import HomeScreen from '../app/screens/Home'
 
 const Routes = () => {
   return (
